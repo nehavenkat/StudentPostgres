8fde133dd89d524d791123905dc0ac736f877b6d
neha venkat <venkatsetty.neha@gmail.com>
2020-01-15 19:42:37 +0100

    post,put and delete

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ dotenv.config();
 const server = express();
 const db = require('./db');
 const studentRouter= require("./src/routes/students")
-
+// TO POST DATA WE NEED TO USE MIDDLEWARE
+server.use(express.json());
  server.use('/students',studentRouter)
 //route for server
 server.get('/',async (req,res) =>
 {
-  const response = await db.query("SELECT * FROM student")
-  res.send(response.rows)
+const response = await db.query("SELECT * FROM student")
+res.send(response.rows)
 }
- )
+)
 
 server.listen(process.env.PORT, () =>console.log(`Server is listening on ${process.env.PORT}`))// install dotenv
--- src/routes/students/index.js
+++ src/routes/students/index.js
@@ -9,7 +9,6 @@ router.get('/', async (req,res)=> {
 
 })
 //avoiding SQL Injection  we are using Parameters//Sanification
-//
 router.get('/:id', async (req,res)=> {
     try {
         const students = await db.query(`SELECT * FROM student WHERE _id =$1`, [req.params.id])
@@ -18,10 +17,54 @@ router.get('/:id', async (req,res)=> {
         else
                return res.send(students.rows[0])
     } catch (error) {
-        res.status(500).send(ex)
-        
+        res.status(500).send(error)    
     }
-   
+})
+
+router.post('/', async (req,res) => {
+try {
+    const result = await db.query(`INSERT INTO student (_id,name,surname,email,dob)
+                                   VALUES($1,$2,$3,$4,$5)
+                                   RETURNING *`,
+                                   [req.body._id, req.body.name,req.body.surname,req.body.email,req.body.dob])
+    res.send(result.rows[0])                               
+} catch (error) {
+    res.status(500).send(error)
+}
+})
+router.put("/:id", async (req, res) => {
+    try {
+        const result = await db.query(`UPDATE student SET 
+                                       name= $1,
+                                       surname = $2,
+                                       email= $3,
+                                       dob= $4 
+                                       WHERE _id= $5`,
+            [req.body.name, req.body.surname, req.body.email, req.body.dob, req.params.id]);
+
+        if (result.rowCount === 0)
+            res.status(404).send("not found")
+        else
+            res.send("OK")
+    }
+    catch (ex) {
+        res.status(500).send(ex)
+    }
+})
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const result = await db.query(`DELETE FROM student WHERE _id = $1`, [req.params.id])
+
+        if (result.rowCount === 0)
+            res.status(404).send("not found")
+        else
+            res.send("OK")
+    }
+    catch (ex) {
+        res.status(500).send(ex)
+    }
 })
 
 
