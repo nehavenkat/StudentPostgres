269963ea15a7281a72ca3ed7a8776755d1a3f37f
neha venkat <venkatsetty.neha@gmail.com>
2020-01-16 16:42:54 +0100

    completed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const listEndpoints = require("express-list-endpoints");
+
 dotenv.config();
 const server = express();
 const db = require('./db');
 const studentRouter= require("./src/routes/students")
 // TO POST DATA WE NEED TO USE MIDDLEWARE
 server.use(express.json());
- server.use('/students',studentRouter)
+server.use('/students',studentRouter)
 //route for server
 server.get('/',async (req,res) =>
 {
@@ -14,5 +16,6 @@ const response = await db.query("SELECT * FROM student")
 res.send(response.rows)
 }
 )
+console.log(listEndpoints(server));
 
 server.listen(process.env.PORT, () =>console.log(`Server is listening on ${process.env.PORT}`))// install dotenv
--- src/routes/students/index.js
+++ src/routes/students/index.js
@@ -1,36 +1,78 @@
 const express = require("express")
-const db= require("../../../db")
+const db = require("../../../db")
 
 const router = express.Router()
 
-router.get('/', async (req,res)=> {
-    const students = await db.query("SELECT * FROM student")
-   res.send(students.rows);
+router.get("/", async (req, res) => {
+    let query = "SELECT * FROM student";
+    const limit = req.query.limit; 
+    const offset = req.query.offset;
+    const sort = req.query.sort;
+    
+    delete req.query.limit
+    delete req.query.offset
+    delete req.query.sort
 
-})
-//avoiding SQL Injection  we are using Parameters//Sanification
-router.get('/:id', async (req,res)=> {
+    const params = [ limit, offset ]  
+
+    let i = 0;
+    for (var propName in req.query) { 
+        query += i === 0 ?  " WHERE " : " AND " 
+        query += propName + " = $" + (i + 3) 
+        params.push(req.query[propName]) 
+        i++;
+    }
+
+    if (sort)
+        query += ' ORDER BY name ' + ((sort === "desc") ? "DESC" : "ASC");
+        query += ' LIMIT $1 OFFSET $2' 
+
     try {
-        const students = await db.query(`SELECT * FROM student WHERE _id =$1`, [req.params.id])
-        if (students.rowCount === 0)
-               return res.status(404).send("Not Found");
-        else
-               return res.send(students.rows[0])
-    } catch (error) {
-        res.status(500).send(error)    
+        const  = await db.query(query, params)
+        res.send(students.rows)
+    }
+    catch (ex) {
+        res.status(500).send(ex)
     }
 })
 
-router.post('/', async (req,res) => {
-try {
-    const result = await db.query(`INSERT INTO student (_id,name,surname,email,dob)
+//avoiding SQL Injection  we are using Parameters//Sanification
+router.get('/:id', async (req, res) => {
+    try {
+        const students = await db.query(`SELECT * FROM student WHERE _id =$1`, [req.params.id])
+        if (students.rowCount === 0)
+            return res.status(404).send("Not Found");
+        else
+            return res.send(students.rows[0])
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//  GET students/:id/projects => returns a list of projects in which also the info from the students appears (use JOIN to retrieve those info)
+router.get('/:id/projects', async (req, res) => {
+    try {
+        const students = await db.query(`SELECT * FROM project JOIN student ON student_id=student._id WHERE student._id =$1`, [req.params.id])
+        if (students.rowCount === 0)
+            return res.status(404).send("NOT FOUND");
+        else
+            return res.send(students.rows[0])
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+
+router.post('/', async (req, res) => {
+    try {
+        const result = await db.query(`INSERT INTO student (_id,name,surname,email,dob)
                                    VALUES($1,$2,$3,$4,$5)
                                    RETURNING *`,
-                                   [req.body._id, req.body.name,req.body.surname,req.body.email,req.body.dob])
-    res.send(result.rows[0])                               
-} catch (error) {
-    res.status(500).send(error)
-}
+            [req.body._id, req.body.name, req.body.surname, req.body.email, req.body.dob])
+        res.send(result.rows[0])
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 router.put("/:id", async (req, res) => {
     try {
